b384183a27c63e629e844f000790dde8ed93cae6
Dheeraj <asaidheeraj@gmail.com>
2021-07-17 11:00:13 +0530

    changes done

--- delete1.sql
+++
@@ -1,4 +0,0 @@
-delete from books_subjects
-where books_subjects.subject = 9;
-delete from subjects
-where subjects.name = 'History';
--- query3.sql
+++
@@ -1,6 +0,0 @@
-select b.title from books as b
-inner join books_subjects as bs
-on b.id=bs.book
-inner join subjects as s
-on bs.subject=s.id
-where s.name IN ('Technology','Politics');
